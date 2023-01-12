1dd46d485edfe00ca9ef7d2f8c250065332fbf11
coderchaubey <sunnychaubeysm@gmail.com>
2023-01-13 00:32:50 +0530

    updated polymorphism uses

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
+class Product{
+    public int product(int x,int y){
+        return x;
+    }
+    public int product(int x,int y,int z){
+        return x;
+    }
+    public double product(double x, double y) {
+        return x;
+    }
+}
 public class Main {
-
+    public static void main(String[] args) {
+        Product p=new Product();
+        p.product(1,2);
+        p.product(1,2,3);
+        p.product(1.2,3.5);
+    }
 }
